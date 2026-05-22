703c8312d60c6f7791cd7e00b4e7c6d94c2cc970
01124833532mo <mohammedhamdi726@gmail.com>
2026-05-22 17:29:45 +0300

    Add RabbitMQ publisher and dependency injection
    Added `IRabbitMQPublisher` interface and its implementation
    `RabbitMQPublisher` to the `BussnissLogicLayer` project. Updated
    `DependencyInjection` to register `RabbitMQPublisher` using
    `AddTransient`.
    
    Implemented RabbitMQ connection setup in `RabbitMQPublisher`,
    including configuration for connection parameters and creation
    of a durable exchange (`products.exchange`). Added a `Publish<T>`
    method to serialize and publish messages to RabbitMQ.
    
    Modified `RabbitMQPublisher` to implement `IDisposable` for
    proper cleanup of RabbitMQ connection and channel resources.

--- BussnissLogicLayer/DependencyInjection.cs
+++ BussnissLogicLayer/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BussnissLogicLayer.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BussnissLogicLayer
 {
@@ -7,6 +8,7 @@ namespace BussnissLogicLayer
         public static IServiceCollection AddBusinessLogicLayerRegistration(this IServiceCollection services)
         {
             services.AddScoped<ServiceContracts.IProductsService, Services.ProductsService>();
+            services.AddTransient<IRabbitMQPublisher, RabbitMQPublisher>();
             return services;
         }
     }
